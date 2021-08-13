26273216345f18d5f172f6f52662e03c03836c83
Federico Pentivolpe <fpentivolpe@googlemail.com>
2021-08-12 22:28:48 -0300

    seguimos haciendo cositas

--- app.js
+++ app.js
@@ -1,25 +1,38 @@
 const prompt = require("prompt-sync")();
-
+const usuarios = require("./funcionesAux")
 
 console.log("Bienvenido")
-console.log("Que desea hacer")
-console.log("Para iniciar sesión 1")
-console.log("Para registrarse 2")
 
-let opcion = parseInt(prompt("Numero opción elegida:"));
+while(true){
 
-if(opcion === 1){
-    console.log("Inicio sesion")
-    const email = prompt("Email:");
-    const pass = prompt("Contraseña:");
+    console.log("Que desea hacer")
+    console.log("Para iniciar sesión 1")
+    console.log("Para registrarse 2")
+    console.log("Para cerrar programa 3")
 
-    if(verificarUsuario(email, pass)){
-        console.log("Usuario existe")
-    }else {
-        console.log("Usuario no existe")
+
+    let opcion = parseInt(prompt("Numero opción elegida:"));
+
+    if(opcion === 1){
+        console.log("Inicio sesion")
+        const email = prompt("Email:");
+        usuarios.verificarEmail(email);
+        const pass = prompt("Contraseña:");
+        if(usuarios.verificar(email, pass)){
+            console.log("Usuario existe")
+        }else {
+            console.log("Usuario no existe")
+        }
+    }else if(opcion === 2){
+        console.log("Registro")
+        const email = prompt("Email:");
+        usuarios.verificarEmail(email);
+        const pass = prompt("Contraseña:");
+        usuarios.registrar(email,pass);
+    }else if(opcion === 3){
+        process.kill(process.pid, "SIGTERM");
+    }else{
+        console.log("Ingreso erroneo")
     }
-}else if(opcion === 2){
-    //registro
-}else{
-    console.log("Ingreso erroneo")
+
 }
--- funcionesAux.js
+++ funcionesAux.js
@@ -1,7 +1,19 @@
+var validator = require("validator");
+
+
 function verificarUsuario(email, pass){
     return true;
 }
 
 function crearUsuario(email, pass){
-    //
+    return true
 }
+
+function verificarEmail(email){
+    if(!validator.isEmail(email)){
+        console.log("Email no valido")
+    }
+}
+exports.verificarEmail = verificarEmail
+exports.verificar = verificarUsuario
+exports.registrar = crearUsuario
